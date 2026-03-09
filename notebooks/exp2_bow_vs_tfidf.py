3f61473f04f8395fd28874cf4e4a543b5228324d
thxrun <tharunkarthik2227@gmail.com>
2026-03-09 13:11:34 +0530

    Mlfow and DVC - Setup

--- notebooks/exp2_bow_vs_tfidf.py
+++ notebooks/exp2_bow_vs_tfidf.py
@@ -28,9 +28,9 @@ warnings.filterwarnings("ignore")
 CONFIG = {
     "data_path": "notebooks/data.csv",
     "test_size": 0.2,
-    "mlflow_tracking_uri": "https://dagshub.com/vikashdas770/YT-Capstone-Project.mlflow",
-    "dagshub_repo_owner": "vikashdas770",
-    "dagshub_repo_name": "YT-Capstone-Project",
+    "mlflow_tracking_uri": "https://dagshub.com/tharunkarthik2227/Capstone-Project---Mlops.mlflow",
+    "dagshub_repo_owner": "tharunkarthik2227",
+    "dagshub_repo_name": "Capstone-Project---Mlops",
     "experiment_name": "Bow vs TfIdf"
 }
 
--- notebooks/exp3_lor_bow_hp.py
+++ notebooks/exp3_lor_bow_hp.py
@@ -21,8 +21,8 @@ warnings.filterwarnings("ignore")
 # os.environ["MLFLOW_DISABLE_ARTIFACTS_DOWNLOAD"] = "1"
 
 # Set MLflow Tracking URI & DAGsHub integration
-MLFLOW_TRACKING_URI = "https://dagshub.com/vikashdas770/YT-Capstone-Project.mlflow"
-dagshub.init(repo_owner="vikashdas770", repo_name="YT-Capstone-Project", mlflow=True)
+MLFLOW_TRACKING_URI = "https://dagshub.com/tharunkarthik2227/Capstone-Project---Mlops.mlflow"
+dagshub.init(repo_owner="tharunkarthik2227", repo_name="Capstone-Project---Mlops", mlflow=True)
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 mlflow.set_experiment("LoR Hyperparameter Tuning")
 
